924027fece63f9780646fdcb1fbf99323d1bd5c0
Denner Portuguez Villalobos <dennerportuguez@gmail.com>
2025-03-16 12:02:43 -0500

    Configure MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@mui/material";
+import theme from '@/muiTheme';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <ThemeProvider theme={theme}>
+        <body>
+          {children}
+        </body>
+      </ThemeProvider>
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
     </html>
   );
 }
---
+++ src/muiTheme.ts
@@ -0,0 +1,14 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#048853',
+      light: '#F3FFF4',
+      dark:'#035F3A',
+    }
+  }
+});
+
+export default theme;
