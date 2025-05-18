62dd750772d169ec30b835c242ebbc3311075781
Denner Portuguez Villalobos <dennerportuguez@gmail.com>
2025-05-17 21:25:32 -0600

    Create basic layout for the home page

--- src/app/(container)/home/page.tsx
+++ src/app/(container)/home/page.tsx
@@ -1,14 +1,42 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { FC } from "react";
 
-export default function Home() {
+interface HomePageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+const HomePage: FC<HomePageProps> = async ({ searchParams }) => {
+  const show = (await searchParams).show || '6';
+
+  // TODO: Use the show const to dinamically render the properties cards.
+  console.log('show', show)
+
   return (
-    <Box>
-      <Typography
-        component={'h1'}
-        sx={{ color: 'primary.main', fontSize: '32px' }}
-      >
-        Welcome to NextJS + MUI introduction.
-      </Typography>
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        backgroundColor: '#F3FFF4'
+      }}
+    >
+      {/* Title section  */}
+      <Stack direction={'row'} justifyContent={'center'} sx={{ py: 3 }}>
+        <Typography
+          component={'h1'}
+          sx={{
+            fontSize: { xs: '30px', md: '48px' },
+            color: '#048853',
+            textAlign: 'center',
+          }}
+        >
+          Recomendaciones de Propiedades
+        </Typography>
+      </Stack>
     </Box>
   );
 }
+
+export default HomePage;
