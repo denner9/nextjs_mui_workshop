46fa296d1eb782f8c6fdeeb79717dc1f24a6e51f
Denner Portuguez Villalobos <dennerportuguez@gmail.com>
2025-05-18 16:21:36 -0600

    Create Property Card

---
+++ src/app/components/InformationWithIcon.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react'
+import { Stack, Typography } from '@mui/material';
+import Image from 'next/image';
+import AreaIcon from '&/images/areaIcon.png';
+import BathIcon from '&/images/bathIcon.png';
+import BedIcon from '&/images/bedIcon.png';
+
+
+interface InformationWithIconProps {
+  icon: 'rooms' | 'bathrooms' | 'area';
+  value: string;
+  description: string;
+}
+
+const InformationWithIcon: FC<InformationWithIconProps> = ({ icon, description, value }) => {
+  let srcImage = BedIcon;
+
+  if (icon === 'area') {
+    srcImage = AreaIcon;
+  } else if (icon === 'bathrooms') {
+    srcImage = BathIcon;
+  }
+
+  return (
+    <Stack>
+      <Stack gap={'4px'}>
+        <Image
+          alt='icon'
+          src={srcImage}
+          height={25}
+          width={25}
+        />
+        <Typography
+          sx={{
+            fontSize: '14px',
+            fontWeight: 500,
+          }}
+        >
+          {value}
+        </Typography>
+      </Stack>
+      <Typography fontSize={'12px'}>{description}</Typography>
+    </Stack>
+  )
+}
+
+export default InformationWithIcon
